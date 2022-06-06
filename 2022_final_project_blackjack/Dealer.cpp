906e220eff205e09f65c96d7f31d8ba612f23ebd
ExyKnox <hskhsk22000@gmail.com>
2022-06-06 14:04:47 +0900

    불필요 메소드 및 주석 제거

--- 2022_final_project_blackjack/Dealer.cpp
+++ 2022_final_project_blackjack/Dealer.cpp
@@ -21,22 +21,20 @@ void Dealer::fair_play_strategy(Player& player) {
 	// 후자 쪽이 더 좋은 방법이긴 할 것 같다.
 
 	// 딜러 카드가 2에서 9 사이일때 히트
-	////////////////////////////////////////////////////////////////딜러 카드가 2에서 9 사이일때 사용자의 상태를 히트로
+	//딜러 카드가 2에서 9 사이일때 사용자의 상태를 히트로
 	// string으로 판단하면 안 될 것 같은데... stoi()??
-	if (2 <= stoi(nowCard) && stoi(nowCard) <= 9)////////////////////////////////////////////stoi 사용했습니다.
+	if (2 <= stoi(nowCard) && stoi(nowCard) <= 9)
 	{
-		player.hit_true();
-
-		player.stand_false();
+		hit = true;
+		stand = false;
 		//burst = false;
 	}
 	// 딜러카드가 7, 10, or 에이스 일 경우 스탠드 
-	/////////////////////////딜러 카드가 10 혹은 에이스 일 경우 사용자의 상태를 스탠드
+	//딜러 카드가 10 혹은 에이스 일 경우 사용자의 상태를 스탠드
 	else if (nowCard == "10" || nowCard == "A")
 	{
-		player.hit_false();
-
-		player.stand_true();
+		hit = false;
+		stand = true;
 		//burst = false;
 	}
 }
--- 2022_final_project_blackjack/Player.h
+++ 2022_final_project_blackjack/Player.h
@@ -40,10 +40,6 @@ public:
 	string endGame(); // STAND, BURST, BLACK JACK 일경우 게임을 종료해주는 함수
 	string select(); // HIT할지 STAND 할지 결정하는 함수 - HIT일 시 true 리턴, STAND일 시 false 리턴
 	string status(); // "stand" "hit" "burst" string 값 리턴 - 딜러용
-	void hit_false();
-	void hit_true();
-	void stand_false();
-	void stand_true();
 };
 
 /* 점수판단
